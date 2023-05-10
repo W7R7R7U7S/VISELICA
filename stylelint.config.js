7a2368b0ec21498a80e5cb21ae7dffc0d8ca8eb6
Maxim Porshnev <gtdm1912@gmail.com>
2023-05-11 01:06:23 +0700

    Create file stylrlint.stylelintrc

--- stylelint.config.js
+++
@@ -1,8 +0,0 @@
-{
-	"rules":	{
-		"color-no-invalid-hex": true,
-		"declaration-block-no-duplicate-properties": true,
-		"selector-pseudo-class-no-unknown": true
-		// Другие правила по вашему выбору
- 	}
-}
